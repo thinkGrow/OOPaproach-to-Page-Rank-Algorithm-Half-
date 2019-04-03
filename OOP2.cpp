5a828b0f16bdd6de3119bf3b248d5932f76acd59
Rubait Reshad <43026478+thinkGrow@users.noreply.github.com>
2019-04-03 19:33:57 +0600

    Add files via upload

---
+++ OOP2.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+
+using namespace std;
+
+class Page
+
+{
+    float rank;
+public :
+    float outgoing[6];
+
+
+
+
+
+
+
+
+
+};
+
+class Matrix
+{
+
+
+
+public:
+    int **data; int row, col;
+    Matrix(){
+        row = col = 0;
+        data = NULL;
+    }
+    //void setMatrix(int x){
+        //row = col = 0;
+        //data = NULL;
+    //}
+
+        void showMatrix(){
+        cout<<endl;
+        if(data==NULL){
+            cout<<"The matrix is not populated. Nothing to show..."<<endl;
+            return;
+        }
+        int i,j;
+        for(i=1;i<=row;i++)
+        {
+            for(j=1;j<=col;j++)
+                cout<<data[i][j]<<" ";
+            cout<<endl;
+        }
+    }
+
+
+
+
+};
+
+int main()
+
+{
+    Page P[6];
+    int ans=0;
+    float countt;
+    float pr;
+    float temp1;
+    float temp2;
+
+
+    for(int i = 1; i<=5; i++)
+    {
+
+       cout << "For P[" << i << "] : " << endl << endl;
+        for(int j=1; j<=5; j++)
+    {
+
+        cout << "Is P[" << i << "] connected to P" << j << "?" ;
+        cin >> ans;
+        if(ans==0){P[i].outgoing[j]=0;cout << "P[" << i << "] is not connected to P" << j << "." << endl;}
+        else {P[i].outgoing[j]=1; cout << "P[" << i << "] connected to P" << j << "." << endl;}
+    }
+        cout << endl;
+    }
+
+    for(int i=1;i<=5;i++)
+    {   for(int j=1;j<=5;j++)
+        {
+        cout << P[j].outgoing[i] << " ";
+        }cout << endl;
+    }
+
+    cout << endl << endl;
+
+
+
+
+
+
+        for(int i=1;i<=5;i++)
+
+        {
+
+        if(P[1].outgoing[i]==1){countt=countt+1;}
+
+        }
+
+        pr=1/countt;
+
+
+
+
+
+
+
+         for(int i=1;i<=5;i++)
+
+
+
+        {
+            if(P[1].outgoing[i]==1)
+                P[1].outgoing[i]=pr;
+
+
+
+        }cout << endl;
+
+
+           for(int i=1;i<=5;i++)
+    {   for(int j=1;j<=5;j++)
+        {
+             cout << P[j].outgoing[i] << "\t";
+
+
+        }cout << endl;
+    }
+
+
+
+
+
+
+
+
+
+    /*Matrix H;
+    H.row = 6;
+    H.col = 6;
+    for (int i=1; i<=5;i++)
+        for(int j=1; j<=5;j++)
+    {
+        if (P[i].outgoing[j]==0)H.data[j][i]=0;
+        else H.data[j][i]=1;
+    }
+    H.showMatrix();*/
+
+}
+
